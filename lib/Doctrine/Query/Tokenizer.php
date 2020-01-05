90bd6a07b1fa0e538fc73e023632767a0065763c
Kurt Moser <kurt@suvaline.com>
2020-01-05 15:45:08 +0200

    Use continue 2 to skip from switch

--- lib/Doctrine/Query/Tokenizer.php
+++ lib/Doctrine/Query/Tokenizer.php
@@ -93,7 +93,7 @@ class Doctrine_Query_Tokenizer
                 break;
 
                 case 'by':
-                    break 2;
+                    continue 2;
 
                 default:
                     if ( ! isset($p)) {
